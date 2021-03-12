3adb5dd9211defcfa20f487a3efc4de0a73b19e0
M2113 <m2113@student.jamk.fi>
2021-03-12 14:58:00 +0200

    test data sql files for products

---
+++ test_data/ProductGategory.sql
@@ -0,0 +1,30 @@
+insert into ProductGategory (ProductGategoryID, ProductGategory, ProductGategoryDescription, ProductGategoryTax, ParentProductGategoryID) values (1, 'Envoy', 'Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.
+
+Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.', 10, null);
+insert into ProductGategory (ProductGategoryID, ProductGategory, ProductGategoryDescription, ProductGategoryTax, ParentProductGategoryID) values (2, 'Dakota', 'Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi. Integer ac neque.', 13, null);
+insert into ProductGategory (ProductGategoryID, ProductGategory, ProductGategoryDescription, ProductGategoryTax, ParentProductGategoryID) values (3, 'Expo', null, 4, null);
+insert into ProductGategory (ProductGategoryID, ProductGategory, ProductGategoryDescription, ProductGategoryTax, ParentProductGategoryID) values (4, 'FJ Cruiser', 'Suspendisse potenti. In eleifend quam a odio. In hac habitasse platea dictumst.', 7, null);
+insert into ProductGategory (ProductGategoryID, ProductGategory, ProductGategoryDescription, ProductGategoryTax, ParentProductGategoryID) values (5, 'X5', 'Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.', 12, null);
+insert into ProductGategory (ProductGategoryID, ProductGategory, ProductGategoryDescription, ProductGategoryTax, ParentProductGategoryID) values (6, 'Legacy', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa. Donec dapibus. Duis at velit eu est congue elementum.', 12, null);
+insert into ProductGategory (ProductGategoryID, ProductGategory, ProductGategoryDescription, ProductGategoryTax, ParentProductGategoryID) values (7, 'F250', null, 13, null);
+insert into ProductGategory (ProductGategoryID, ProductGategory, ProductGategoryDescription, ProductGategoryTax, ParentProductGategoryID) values (8, 'Equinox', null, 11, null);
+insert into ProductGategory (ProductGategoryID, ProductGategory, ProductGategoryDescription, ProductGategoryTax, ParentProductGategoryID) values (9, 'GLK-Class', 'Morbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.
+
+Fusce consequat. Nulla nisl. Nunc nisl.', 5, null);
+insert into ProductGategory (ProductGategoryID, ProductGategory, ProductGategoryDescription, ProductGategoryTax, ParentProductGategoryID) values (10, 'Eclipse', 'In hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus.
+
+Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', 11, null);
+insert into ProductGategory (ProductGategoryID, ProductGategory, ProductGategoryDescription, ProductGategoryTax, ParentProductGategoryID) values (11, 'Amanti', 'Duis consequat dui nec nisi volutpat eleifend. Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus.
+
+Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.', 3, null);
+insert into ProductGategory (ProductGategoryID, ProductGategory, ProductGategoryDescription, ProductGategoryTax, ParentProductGategoryID) values (12, 'Solara', 'Duis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus.
+
+In sagittis dui vel nisl. Duis ac nibh. Fusce lacus purus, aliquet at, feugiat non, pretium quis, lectus.', 22, null);
+insert into ProductGategory (ProductGategoryID, ProductGategory, ProductGategoryDescription, ProductGategoryTax, ParentProductGategoryID) values (13, 'CX-9', null, 1, null);
+insert into ProductGategory (ProductGategoryID, ProductGategory, ProductGategoryDescription, ProductGategoryTax, ParentProductGategoryID) values (14, 'ES', null, 9, null);
+insert into ProductGategory (ProductGategoryID, ProductGategory, ProductGategoryDescription, ProductGategoryTax, ParentProductGategoryID) values (15, 'Skylark', 'Morbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.', 3, null);
+insert into ProductGategory (ProductGategoryID, ProductGategory, ProductGategoryDescription, ProductGategoryTax, ParentProductGategoryID) values (16, 'New Beetle', 'In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.', 23, null);
+insert into ProductGategory (ProductGategoryID, ProductGategory, ProductGategoryDescription, ProductGategoryTax, ParentProductGategoryID) values (17, 'Savana', null, 12, null);
+insert into ProductGategory (ProductGategoryID, ProductGategory, ProductGategoryDescription, ProductGategoryTax, ParentProductGategoryID) values (18, 'CC', null, 24, null);
+insert into ProductGategory (ProductGategoryID, ProductGategory, ProductGategoryDescription, ProductGategoryTax, ParentProductGategoryID) values (19, 'F-Series', 'Morbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis.', 10, null);
+insert into ProductGategory (ProductGategoryID, ProductGategory, ProductGategoryDescription, ProductGategoryTax, ParentProductGategoryID) values (20, 'Aero 8', 'Curabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.', 13, null);
