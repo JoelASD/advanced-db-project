e7f649d6c2e4fd87779dbfe7213d3f768371e2d8
M2113 <m2113@student.jamk.fi>
2021-03-12 15:09:09 +0200

    test data sql files for bundles

---
+++ test_data/Bundle.sql
@@ -0,0 +1,15 @@
+insert into Bundle (BundleID, BundleName, BundleInfo, BundleDiscount) values (1, 'Cookley', null, 24);
+insert into Bundle (BundleID, BundleName, BundleInfo, BundleDiscount) values (2, 'Quo Lux', null, 25);
+insert into Bundle (BundleID, BundleName, BundleInfo, BundleDiscount) values (3, 'Hatity', null, 13);
+insert into Bundle (BundleID, BundleName, BundleInfo, BundleDiscount) values (4, 'Mat Lam Tam', null, 28);
+insert into Bundle (BundleID, BundleName, BundleInfo, BundleDiscount) values (5, 'Span', 'morbi sem mauris laoreet ut rhoncus aliquet pulvinar', 23);
+insert into Bundle (BundleID, BundleName, BundleInfo, BundleDiscount) values (6, 'Flowdesk', 'natoque penatibus et magnis dis parturient montes nascetur ridiculus mus', 45);
+insert into Bundle (BundleID, BundleName, BundleInfo, BundleDiscount) values (7, 'Asoka', 'pellentesque at nulla suspendisse potenti cras in purus', 23);
+insert into Bundle (BundleID, BundleName, BundleInfo, BundleDiscount) values (8, 'Tresom', null, 35);
+insert into Bundle (BundleID, BundleName, BundleInfo, BundleDiscount) values (9, 'Cookley', null, 22);
+insert into Bundle (BundleID, BundleName, BundleInfo, BundleDiscount) values (10, 'Voltsillam', 'sapien varius ut blandit non', 24);
+insert into Bundle (BundleID, BundleName, BundleInfo, BundleDiscount) values (11, 'Stim', 'nullam porttitor lacus at', 17);
+insert into Bundle (BundleID, BundleName, BundleInfo, BundleDiscount) values (12, 'Stringtough', 'lacus at turpis donec posuere metus vitae ipsum aliquam non', 70);
+insert into Bundle (BundleID, BundleName, BundleInfo, BundleDiscount) values (13, 'Zaam-Dox', null, 10);
+insert into Bundle (BundleID, BundleName, BundleInfo, BundleDiscount) values (14, 'Pannier', 'quis augue luctus tincidunt nulla', 25);
+insert into Bundle (BundleID, BundleName, BundleInfo, BundleDiscount) values (15, 'Rank', 'tempus vel pede morbi porttitor lorem id', 41);
